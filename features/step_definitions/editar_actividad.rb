1d3f8e2ecf12ae852b5c8fa41caef43224b03670
alvaro2302 <alvaro230297@hotmail.com>
2020-06-11 20:46:20 -0400

    add 5 features

---
+++ features/step_definitions/editar_actividad.rb
@@ -0,0 +1,59 @@
+Given("entered with a director account") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When("I click on button  {string} at the top") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When("click on button  {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When("click on button {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When("select the PDF {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When("click on  {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When("select career to  {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When("select year to {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When("click on  {string} in the first activity") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When("add date  {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Then("should show the date {string} in the first activity") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When("I click on the button {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When("enter the value {int} in the {string} field") do |int, string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When("click on {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Then("should show activity table") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,18 +1,38 @@
-require File.join(File.dirname(__FILE__), '..', '..', 'app.rb')
-
-require 'capybara'
-require 'capybara/cucumber'
-require 'rspec'
-
-Capybara.app = App
-
-class MyWorld
-  include Capybara::DSL
-
-  def app
-    Sinatra::Application
-  end
+begin require 'rspec/expectations'; rescue LoadError; require 'spec/expectations'; end
+  require 'capybara'
+  require 'capybara/dsl'
+  require 'capybara/cucumber'
+  require 'capybara-screenshot/cucumber'
   
-end
-
-World{MyWorld.new}
+  #PTravel Settings
+  ENV['USER']="Pepazo"
+  ENV['PSW']="ILoveQA"
+  
+  
+  Capybara.default_driver = :selenium
+  
+  # Set the host the Capybara tests should be run against
+  Capybara.app_host = ENV["CAPYBARA_HOST"]
+  
+  # Set the time (in seconds) Capybara should wait for elements to appear on the page
+  Capybara.default_max_wait_time = 15
+  Capybara.default_driver = :selenium
+  Capybara.app_host = "http://www.google.com"
+  
+  class CapybaraDriverRegistrar
+    # register a Selenium driver for the given browser to run on the localhost
+    def self.register_selenium_driver(browser)
+      Capybara.register_driver :selenium do |app|
+        Capybara::Selenium::Driver.new(app, :browser => browser)
+      end
+    end
+  
+  end
+  # Register various Selenium drivers
+  #CapybaraDriverRegistrar.register_selenium_driver(:internet_explorer)
+  #CapybaraDriverRegistrar.register_selenium_driver(:firefox)
+  CapybaraDriverRegistrar.register_selenium_driver(:firefox)
+  Capybara.run_server = false
+  #World(Capybara)
+  
+  
