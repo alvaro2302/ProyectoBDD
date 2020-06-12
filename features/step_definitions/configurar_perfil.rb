33a19be72ea0962a87c7822e66c28e898398dfe1
alvaro2302 <alvaro230297@hotmail.com>
2020-06-12 18:49:52 -0400

    add feature from cargar PDF and table activity

---
+++ features/step_definitions/configurar_perfil.rb
@@ -0,0 +1,19 @@
+When("I click on button {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+When("entered the field {string} to {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When("entered the field {string} and {string} to {string}") do |string, string2, string3|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When("Click on the button {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Then("should show me a message {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ features/step_definitions/tabla_de_actividades.rb
@@ -0,0 +1,27 @@
+Given("entered with a DTC account") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click at the top in {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("select the career of {string} and the year {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click on button update for {int} times") do |int|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("should show {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("Hago on button update for {int} times") do |int|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("should show in camp {string} for  {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
